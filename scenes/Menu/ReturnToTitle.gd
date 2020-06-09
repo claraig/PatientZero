afb69681e4721e83ce1c49c70394084b955f0f4b
GabrielAdamski <gabriel.adamski.oliveira@gmail.com>
2020-06-09 19:17:27 -0300

    adicionado regra dos 40 porcento

--- scenes/Menu/ReturnToTitle.gd
+++ scenes/Menu/ReturnToTitle.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 
-#func _ready() -> void:
+func _ready() -> void:
+	atualizaValorDificuldade()
 #	pass # Replace with function body.
 
 
@@ -13,3 +14,29 @@ extends Node2D
 func _on_Button_pressed():
 	get_node('audio').play()
 	get_tree().change_scene("res://scenes/Menu.tscn")
+	
+	
+func atualizaValorDificuldade():
+	var dificuldade = get_node("/root/Configuracoes").dificuldade
+	var labelValorDificuldade = get_node("valorDificuldade")
+	if (dificuldade == 0):
+		labelValorDificuldade.text = "Normal"
+		labelValorDificuldade.add_color_override("font_color", Color(0.212, 0.608, 0.145))
+	elif (dificuldade == 1):
+		labelValorDificuldade.text = "Difícil"
+		labelValorDificuldade.add_color_override("font_color", Color(0.835, 0.859, 0.212))
+	else:
+		labelValorDificuldade.text = "Muito Difícil"
+		labelValorDificuldade.add_color_override("font_color", Color(0.776, 0, 0))
+		
+		
+
+
+func _on_aumentaDificuldade_pressed():
+	 get_node("/root/Configuracoes").adicionarDificuldade()
+	 atualizaValorDificuldade()
+
+
+func _on_diminuiDificuldade_pressed():
+	 get_node("/root/Configuracoes").diminuirDificuldade()
+	 atualizaValorDificuldade()
--- scripts/configuracoes.gd
+++ scripts/configuracoes.gd
@@ -2,19 +2,24 @@ extends Node
 
 
 # Declare member variables here. Examples:
-var dificuldade:int = 1
+var dificuldade:int = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	dificuldade = 2
+	dificuldade = 0
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 
-func adicionaDificuldade():
+func adicionarDificuldade():
 	dificuldade+=1
 	if(dificuldade > 2):
-		dificuldade = 0;
+		dificuldade = 0
+		
+func diminuirDificuldade():
+	dificuldade-=1
+	if(dificuldade < 0):
+		dificuldade = 2;
