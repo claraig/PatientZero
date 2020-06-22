519601eeb88dee5e45c95bd12f81058d335daa3b
Ghost Clara <claraig91@gmail.com>
2020-06-22 16:59:17 -0300

    agora funciona avatar pra todos os niveis de dificuldade. também botão pra efeitos sonoros nas opções

--- scenes/Menu/ReturnToTitle.gd
+++ scenes/Menu/ReturnToTitle.gd
@@ -1,13 +1,29 @@
 extends Node2D
 
+var silence: bool
+
+
+
 func _ready() -> void:
 	atualizaValorDificuldade()
+#	silence = get_node("/root/Configuracoes").temsom
+	print(silence)
+#	get_node('CheckButton').toggle_mode(silence)
+#	get_node('CheckButton').pressed = silence
 
 
-func _on_Button_pressed():
-	get_node('audio').play()
-	get_tree().change_scene("res://scenes/Menu.tscn")
+#	AUDIO
 
+func _on_CheckButton_pressed():
+	get_node("/root/Configuracoes").ajustarSom()
+	AudioServer.set_bus_mute(0, get_node("/root/Configuracoes").temsom)
+	silence = get_node('CheckButton').pressed
+	print(silence)
+
+
+
+
+#	DIFICULDADE
 
 func atualizaValorDificuldade():
 	var dificuldade = get_node("/root/Configuracoes").dificuldade
@@ -22,14 +38,22 @@ func atualizaValorDificuldade():
 		labelValorDificuldade.text = "Muito Difícil"
 		labelValorDificuldade.add_color_override("font_color", Color(0.776, 0, 0))
 
-
-
-
 func _on_aumentaDificuldade_pressed():
-	 get_node("/root/Configuracoes").adicionarDificuldade()
-	 atualizaValorDificuldade()
-
+	get_node('audio').play()
+	get_node("/root/Configuracoes").adicionarDificuldade()
+	atualizaValorDificuldade()
 
 func _on_diminuiDificuldade_pressed():
-	 get_node("/root/Configuracoes").diminuirDificuldade()
-	 atualizaValorDificuldade()
+	get_node('audio').play()
+	get_node("/root/Configuracoes").diminuirDificuldade()
+	atualizaValorDificuldade()
+
+
+
+
+# SAIR
+
+func _on_Button_pressed():
+	get_node('svoltar').play()
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://scenes/Menu.tscn")
--- scripts/configuracoes.gd
+++ scripts/configuracoes.gd
@@ -1,25 +1,23 @@
 extends Node
 
-
-# Declare member variables here. Examples:
 var dificuldade:int = 0
+var temsom:bool
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	dificuldade = 0
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
 
 func adicionarDificuldade():
 	dificuldade+=1
 	if(dificuldade > 2):
 		dificuldade = 0
-		
+
 func diminuirDificuldade():
 	dificuldade-=1
 	if(dificuldade < 0):
 		dificuldade = 2;
+
+func ajustarSom():
+	temsom = !temsom
+#	if temsom:
+#		print("mudou som")
