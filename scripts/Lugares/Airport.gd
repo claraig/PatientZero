e3a50248a7a6f6ed156cee50e14b9255325a82a9
Ghost Clara <claraig91@gmail.com>
2020-06-21 23:55:46 -0300

    remoção de assets velhos. avatares funcionando no nível de dificuldade normal

--- scripts/Lugares/Airport.gd
+++ scripts/Lugares/Airport.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 var visitantes = []
 var texto: String
 
@@ -9,11 +8,9 @@ var texto: String
 func _ready():
 	pass # Replace with function body.
 
-
 func formatarVisitantes():
 	for dado in visitantes:
 		texto += "Dia: "+ str(dado.turno)+ "\nVisitantes: " + str(dado.pessoas)+ "\n"
-
 
 func _on_Button_button_down() -> void:
 	visitantes.push_back({"dia":1, "pessoas":"bia, ze, marco"})
